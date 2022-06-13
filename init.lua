60aafe8125a733fd4c1efa8c749de308688ae38b
Fola Flor <fola_studio@Folaui-Mac-Studio.local>
2022-06-13 22:25:19 +0900

    First Commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- hs.alert.show('Hello Mammerspoon!!')
+
+-- Alert message
+function HelloHammerspoon() 
+    hs.alert.show('Hello Mammerspoon!!') 
+end
+
+
+hs.hotkey.bind(
+    {'shift', 'cmd'}, 'H', HelloHammerspoon)
+    
